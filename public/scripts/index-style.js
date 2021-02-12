24f4a19a21389ac5dbab372fa198522a9a57453b
ccrudkin <ccrudkin@gmail.com>
2021-02-12 16:46:44 -0700

    ff

--- public/scripts/index-style.js
+++ public/scripts/index-style.js
@@ -1,8 +1,8 @@
 $(document).ready(() => {
-    if ($('#unitAB').innerText === 'a') {
+    if ($('#unitAB').innerText() === 'a') {
         $('#btn-alpha-container').addClass('button-current');
         $('#btn-beta-container').removeClass('button-current');
-    } else if ($('#unitAB').innerText === 'b') {
+    } else if ($('#unitAB').innerText() === 'b') {
         $('#btn-beta-container').addClass('button-current');
         $('#btn-alpha-container').removeClass('button-current');
     }
