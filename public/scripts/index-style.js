ad5f2cebe6f4a00ee5183531a775f84c919e0741
ccrudkin <ccrudkin@gmail.com>
2021-02-12 16:59:37 -0700

    button border-bottom

--- public/scripts/index-style.js
+++ public/scripts/index-style.js
@@ -1,8 +1,8 @@
 $(document).ready(() => {
-    if ($('#unitAB').innerText() === 'a') {
+    if ($('#unitAB').text() === 'a') {
         $('#btn-alpha-container').addClass('button-current');
         $('#btn-beta-container').removeClass('button-current');
-    } else if ($('#unitAB').innerText() === 'b') {
+    } else if ($('#unitAB').text() === 'b') {
         $('#btn-beta-container').addClass('button-current');
         $('#btn-alpha-container').removeClass('button-current');
     }
