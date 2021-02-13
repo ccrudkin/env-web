34befcea22e04c6603ee878be1a59027a01f1077
ccrudkin <ccrudkin@gmail.com>
2021-02-12 17:05:44 -0700

    active button classes

--- public/scripts/index-style.js
+++ public/scripts/index-style.js
@@ -1,9 +1,13 @@
 $(document).ready(() => {
     if ($('#unitAB').text() === 'a') {
-        $('#btn-alpha-container').addClass('button-current');
-        $('#btn-beta-container').removeClass('button-current');
+        $('#btn-alpha-container').addClass('button-primary');
+        $('#btn-alpha-container').removeClass('button-secondary');
+        $('#btn-beta-container').addClass('button-secondary');
+        $('#btn-beta-container').removeClass('button-primary');
     } else if ($('#unitAB').text() === 'b') {
-        $('#btn-beta-container').addClass('button-current');
-        $('#btn-alpha-container').removeClass('button-current');
+        $('#btn-beta-container').addClass('button-primary');
+        $('#btn-beta-container').removeClass('button-secondary');
+        $('#btn-alpha-container').addClass('button-secondary');
+        $('#btn-alpha-container').removeClass('button-primary');
     }
 });
